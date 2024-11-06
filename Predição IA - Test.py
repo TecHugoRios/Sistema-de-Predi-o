a09ec9803055f078e76e907f21562628e16688af
Hugo <hugoba532@gmail.com>
2024-11-05 23:39:14 -0300

    dados definitivos a serem utilizados

--- Predição IA - Test.py
+++ Predição IA - Test.py
@@ -1,28 +1,28 @@
 import pandas as pd
 
-df = pd.read_excel('plastic-pollution (1).xlsx')
+df = pd.read_excel('plastic-waste-generation.xlsx')
 
 dfAmerica = df[df['Entity'] == 'Americas (excl. USA)']
-#years = dfAmerica['Year']
+dfUSA = df[df['Entity'] == 'United States']
+years = list(dfAmerica['Year'])
 
 print(dfAmerica)
+print(dfUSA)
 
 print("Americas (excl. USA): ")
-years = [2010,2019]
-mismanaged_str = list(dfAmerica["Mismanaged(T)"])
+#years = [2010,2019]
+total_waste_str = list(dfAmerica["Total waste"])
 
-mismanaged_transform = [int(value.replace(",", "")) for value in mismanaged_str]
+total_waste = [value for year, value in zip(years,total_waste_str)]
 
-mismanaged = [value for year, value in zip(years,mismanaged_transform)]
-
-for year, value in zip(years,mismanaged):
+for year, value in zip(years,total_waste):
     print(f"Ano {year}: {value:,.0f} t")
 
-delta_mismanaged = mismanaged[-1] - mismanaged[0]
+delta_total_waste = total_waste[-1] - total_waste[0]
 delta_years = years[-1] - years[0]
-a = delta_mismanaged / delta_years
+a = delta_total_waste / delta_years
 
-b = mismanaged[0] - a * years[0]
+b = total_waste[0] - a * years[0]
 
 future_years = list(range(2020, 2050))
 predictions = [a * year + b for year in future_years]
@@ -32,12 +32,13 @@ for year, prediction in zip(future_years, predictions):
     #print(prediction)
 
 years_list = []
-mismanaged_list =  []
+total_waste_list =  []
 for index,row in dfAmerica.iterrows():
     year = row['Year']
-    mismanaged = row['Mismanaged(T)']
+    total_waste = row['Total waste']
     years_list.append(year)
-    mismanaged_list.append(mismanaged)
+
+'''    total_waste_list.append(total_waste)
 
 import os
 
@@ -52,16 +53,18 @@ client = Groq(
 
 chat_completion = client.chat.completions.create(
     messages=[
+
         {
             "role": "system",
-            "content": "You are a system that predict quantity of waste of plastic for future years based in a dictionary of years and respective Mismanaged(T). Generate a dictionary of future years and the respective prediction.Write in that form '{'year': 2020, 'mismanaged(T)': 5,591,629 t}'.Explain to me how you arrived at this prediction.Do not put any other additional information",
+            "content": "You are a system that predict quantity of waste of plastic for future years based in a dictionary of years and respective total_waste(T). Generate a dictionary of future years and the respective prediction.Write in that form '{'year': 2020, 'total_waste(T)': 5,591,629 t}'.Explain to me how you arrived at this prediction.Do not put any other additional information",
         },
         {
             "role": "user",
-            "content": f"years: {years_list}, Quantity: {mismanaged_list} ",
+            "content": f"years: {years_list}, Quantity: {total_waste_list} ",
         }
     ],
     model="llama3-8b-8192",
 )
 
 print(chat_completion.choices[0].message.content)    
+'''
